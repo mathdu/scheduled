10db4382220261d26a1f7c855f630f751df7b2c5
Mathieu Duchesneau <mduchesneau@gmail.com>
2015-04-06 22:56:51 -0400

    add import file upload to Web

--- Mduchesneau.Scheduled.Web/app.js
+++ Mduchesneau.Scheduled.Web/app.js
@@ -1,8 +1,15 @@
 ﻿(function () {
-    angular.module('schedule', ['schedule-calendar', 'schedule-api'])
+    angular.module('schedule', ['schedule-calendar', 'schedule-api', 'file-upload'])
 
-    .controller('ImportController', ['$scope', function($scope) {
-        
+    .controller('ImportController', ['$scope', 'fileUpload', function ($scope, fileUpload) {
+        var importer = this;
+
+        importer.uploadFile = function () {
+            var file = $scope.importFile;
+            var uploadUrl = "http://localhost:22249/events/import";
+            fileUpload.uploadFileToUrl(file, uploadUrl);
+        };
+
     }])
 
     .controller('CalendarController', ['$scope', 'configureCalendarUi', 'corsHeaders', function ($scope, configureCalendarUi, corsHeaders) {
@@ -17,7 +24,23 @@
         };
 
         $scope.events = [$scope.eventSource];
-    }]);
+    }])
 
+    .service('fileUpload', ['$http', function ($http) {
+        this.uploadFileToUrl = function (file, uploadUrl) {
+            var fd = new FormData();
+            fd.append('file', file);
+            $http.post(uploadUrl, fd, {
+                transformRequest: angular.identity,
+                headers: { 'Content-Type': undefined }
+            })
+            .success(function () {
+                alert('file uploaded!');
+            })
+            .error(function () {
+                alert('file upload error!');
+            });
+        }
+    }]);
 
 })();
---
+++ Mduchesneau.Scheduled.Web/modules/file-upload.js
@@ -0,0 +1,21 @@
+﻿(function() {
+    angular.module('file-upload', [])
+        
+    // file upload control directive
+    // inspired by: https://uncorkedstudios.com/blog/multipartformdata-file-upload-with-angularjs
+    .directive('simpleFileUpload', ['$parse', function($parse) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                var model = $parse(attrs.simpleFileUpload);
+                var modelSetter = model.assign;
+
+                element.bind('change', function() {
+                    scope.$apply(function() {
+                        modelSetter(scope, element[0].files[0]);
+                    });
+                });
+            }
+        };
+    }]);
+})();
